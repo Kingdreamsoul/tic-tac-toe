6729a3e18aced9c2c4a91021ceddb7717d5f7b76
Kingdreamsoul <35564322+Kingdreamsoul@users.noreply.github.com>
2019-07-30 15:59:28 +0800

    upload build

---
+++ precache-manifest.10840852480779978d7029e6486f9758.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5e2f380e828f224fb76728bc2e16a748",
+    "url": "/tic-tac-toe/index.html"
+  },
+  {
+    "revision": "d148b4f1dbe6de14935d",
+    "url": "/tic-tac-toe/static/css/main.3fa7586b.chunk.css"
+  },
+  {
+    "revision": "685d0c597e8419129599",
+    "url": "/tic-tac-toe/static/js/2.0e3f6a1a.chunk.js"
+  },
+  {
+    "revision": "d148b4f1dbe6de14935d",
+    "url": "/tic-tac-toe/static/js/main.7d32f487.chunk.js"
+  },
+  {
+    "revision": "9728fa41a930f12fdc87",
+    "url": "/tic-tac-toe/static/js/runtime~main.f1e29306.js"
+  }
+]);
